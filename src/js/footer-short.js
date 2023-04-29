2d39a2c8f37484a97f4e84e537ae50405bdc9c50
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-04-29 03:29:47 +0300

    upd main page

---
+++ src/js/footer-short.js
@@ -0,0 +1,9 @@
+const footerNavEl = document.querySelector('.footer__nav');
+
+const footerSocialsEl = document.querySelector('.footer__socials');
+
+footerNavEl.style.display = 'none';
+footerSocialsEl.style.display = 'none';
+
+// console.log(footerNavEl);
+// console.log(footerSocialsEl);

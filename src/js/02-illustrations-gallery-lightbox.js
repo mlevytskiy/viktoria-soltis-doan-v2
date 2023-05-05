3fa375010b9ac379a1428d0dc9b312e61adca7c5
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-05-05 03:03:00 +0300

    проба розібратись з illustrations

--- src/js/02-illustrations-gallery-lightbox.js
+++ src/js/02-illustrations-gallery-lightbox.js
@@ -1,4 +1,5 @@
-// import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { illustrationsGalleryItems } from './gallery-illustrations-items.js';
 
--- src/js/gallery-illustrations-items.js
+++ src/js/gallery-illustrations-items.js
@@ -5,95 +5,95 @@ export const illustrationsGalleryItems = [
     description:
       "Illustration for the Wimmelbook ''Transport'', PET publishing house",
   },
-  {
-    preview: './images/illustrations/Illustration-02_preview.jpg',
-    original: './images/illustrations/Illustration-02.jpg',
-    description:
-      'Illustration for the annual calendar for the Raduga advertising agency',
-  },
-  {
-    preview: './images/illustrations/Illustration-03_preview.jpg',
-    original: './images/illustrations/Illustration-03.jpg',
-    description:
-      "Illustration for the author of the book ''The Boy from Mars''",
-  },
-  {
-    preview: './images/illustrations/Illustration-04_preview.jpg',
-    original: './images/illustrations/Illustration-04.jpg',
-    description:
-      "Illustration for the Wimmelbook ''Kindergarten'',PET publishing house",
-  },
-  {
-    preview: './images/illustrations/Illustration-05_preview.jpg',
-    original: './images/illustrations/Illustration-05.jpg',
-    description: "Illustration for the author of the book ''Go to Bed, Ollie''",
-  },
-  {
-    preview: './images/illustrations/Illustration-06_preview.jpg',
-    original: './images/illustrations/Illustration-06.jpg',
-    description: 'Illustration of a board game for a travel company',
-  },
-  {
-    preview: './images/illustrations/Illustration-07_preview.jpg',
-    original: './images/illustrations/Illustration-07.jpg',
-    description: 'Character development',
-  },
-  {
-    preview: './images/illustrations/Illustration-08_preview.jpg',
-    original: './images/illustrations/Illustration-08.jpg',
-    description: "Illustration for the book ''The Boy from Mars''",
-  },
-  {
-    preview: './images/illustrations/Illustration-09_preview.jpg',
-    original: './images/illustrations/Illustration-09.jpg',
-    description: 'Christmas card to order',
-  },
-  {
-    preview: './images/illustrations/Illustration-10_preview.jpg',
-    original: './images/illustrations/Illustration-10.jpg',
-    description: 'Custom illustration for the book',
-  },
-  {
-    preview: './images/illustrations/Illustration-11_preview.jpg',
-    original: './images/illustrations/Illustration-11.jpg',
-    description: 'Illustrations for the book about Rapunzel',
-  },
-  {
-    preview: './images/illustrations/Illustration-12_preview.jpg',
-    original: './images/illustrations/Illustration-12.jpg',
-    description:
-      'Illustrations for the book about Crocodile, for the author from Belgium',
-  },
-  {
-    preview: './images/illustrations/Illustration-13_preview.jpg',
-    original: './images/illustrations/Illustration-13.jpg',
-    description: 'Christmas illustration to order',
-  },
-  {
-    preview: './images/illustrations/Illustration-14_preview.jpg',
-    original: './images/illustrations/Illustration-14.jpg',
-    description: 'Illustration and cover design for a British publisher',
-  },
-  {
-    preview: './images/illustrations/Illustration-17_preview.jpg',
-    original: './images/illustrations/Illustration-17.jpg',
-    description: 'A full illustrated pug and corgi book for a US author',
-  },
-  {
-    preview: './images/illustrations/Illustration-15_preview.jpg',
-    original: './images/illustrations/Illustration-15.jpg',
-    description:
-      'Complete illustration of books (2 parts) about a crocodile for a writer from Belgium',
-  },
-  {
-    preview: './images/illustrations/Illustration-16_preview.jpg',
-    original: './images/illustrations/Illustration-16.jpg',
-    description:
-      'Complete illustration of books (2 parts) about a crocodile for a writer from Belgium',
-  },
-  {
-    preview: './images/illustrations/Illustration-18_preview.jpg',
-    original: './images/illustrations/Illustration-18.jpg',
-    description: 'A full illustrated book',
-  },
+  // {
+  //   preview: './images/illustrations/Illustration-02_preview.jpg',
+  //   original: './images/illustrations/Illustration-02.jpg',
+  //   description:
+  //     'Illustration for the annual calendar for the Raduga advertising agency',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-03_preview.jpg',
+  //   original: './images/illustrations/Illustration-03.jpg',
+  //   description:
+  //     "Illustration for the author of the book ''The Boy from Mars''",
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-04_preview.jpg',
+  //   original: './images/illustrations/Illustration-04.jpg',
+  //   description:
+  //     "Illustration for the Wimmelbook ''Kindergarten'',PET publishing house",
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-05_preview.jpg',
+  //   original: './images/illustrations/Illustration-05.jpg',
+  //   description: "Illustration for the author of the book ''Go to Bed, Ollie''",
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-06_preview.jpg',
+  //   original: './images/illustrations/Illustration-06.jpg',
+  //   description: 'Illustration of a board game for a travel company',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-07_preview.jpg',
+  //   original: './images/illustrations/Illustration-07.jpg',
+  //   description: 'Character development',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-08_preview.jpg',
+  //   original: './images/illustrations/Illustration-08.jpg',
+  //   description: "Illustration for the book ''The Boy from Mars''",
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-09_preview.jpg',
+  //   original: './images/illustrations/Illustration-09.jpg',
+  //   description: 'Christmas card to order',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-10_preview.jpg',
+  //   original: './images/illustrations/Illustration-10.jpg',
+  //   description: 'Custom illustration for the book',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-11_preview.jpg',
+  //   original: './images/illustrations/Illustration-11.jpg',
+  //   description: 'Illustrations for the book about Rapunzel',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-12_preview.jpg',
+  //   original: './images/illustrations/Illustration-12.jpg',
+  //   description:
+  //     'Illustrations for the book about Crocodile, for the author from Belgium',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-13_preview.jpg',
+  //   original: './images/illustrations/Illustration-13.jpg',
+  //   description: 'Christmas illustration to order',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-14_preview.jpg',
+  //   original: './images/illustrations/Illustration-14.jpg',
+  //   description: 'Illustration and cover design for a British publisher',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-17_preview.jpg',
+  //   original: './images/illustrations/Illustration-17.jpg',
+  //   description: 'A full illustrated pug and corgi book for a US author',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-15_preview.jpg',
+  //   original: './images/illustrations/Illustration-15.jpg',
+  //   description:
+  //     'Complete illustration of books (2 parts) about a crocodile for a writer from Belgium',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-16_preview.jpg',
+  //   original: './images/illustrations/Illustration-16.jpg',
+  //   description:
+  //     'Complete illustration of books (2 parts) about a crocodile for a writer from Belgium',
+  // },
+  // {
+  //   preview: './images/illustrations/Illustration-18_preview.jpg',
+  //   original: './images/illustrations/Illustration-18.jpg',
+  //   description: 'A full illustrated book',
+  // },
 ];
